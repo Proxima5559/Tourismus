95bf9bddbb8a936f4477a5fc16fd3bdf9f1c9202
Proxima5559 <davit.davtyan.0210@gmail.com>
2026-06-01 17:30:37 +0400

    ad: the safest section on scroll feature

--- blueprints/places_dashboard/places_dashboard.py
+++ blueprints/places_dashboard/places_dashboard.py
@@ -4,35 +4,27 @@ from consonants_cs.month_places import MONTHS_HY
 from services.places_service import PlacesService
 from datetime import datetime
 
-
-
 load_dotenv(".env")
 
 places_dashboard_bp = Blueprint('places_dashboard', __name__, template_folder='templates', url_prefix='/places_dashboard')
-
-# @places_dashboard_bp.route('/dashboard/historic')
-# def get_historic():
-#     data = PlacesService.get_dashboard_data()
-#     return render_template("places_dashboard/historic.html", 
-#                            historic_countries=data['historic_countries'])
-    
-# @places_dashboard_bp.route('/dashboard/the_safest')
-# def get_the_safest():
-#     data = PlacesService.get_dashboard_data()
-#     return render_template("places_dashboard/the_safest.html", 
-#                            safe_countries=data['safe_countries'])
-
-@places_dashboard_bp.route('/dashboard/dangerous')
-def get_dangerous():
-    data = PlacesService.get_dashboard_data()
-    return render_template("places_dashboard/dangerous.html", 
-                           dangerous_countries=data['dangerous_countries'])
 
 @places_dashboard_bp.route('/')
 def places_dashboard():
     dashboard_data = PlacesService.get_dashboard_data()
     current_month_idx = datetime.now().month
     return render_template('places_dashboard/places_dashboard.html', months_hy=MONTHS_HY, current_month=current_month_idx, **dashboard_data)
+
+@places_dashboard_bp.route('/dashboard/the_safest')
+def get_the_safest():
+    data = PlacesService.get_dashboard_data()
+    return render_template("places_dashboard/the_safest.html", 
+                           safe_countries=data['safe_countries'])
+
+@places_dashboard_bp.route('/dashboard/dangerous')
+def get_dangerous():
+    data = PlacesService.get_dashboard_data()
+    return render_template("places_dashboard/dangerous.html", 
+                           dangerous_countries=data['dangerous_countries'])
 
 @places_dashboard_bp.route('/country/<country_code>')
 def country_page(country_code):
@@ -46,9 +38,3 @@ def get_seasonal(month):
     return render_template("places_dashboard/seasonal_list.html", 
                            countries=countries, 
                            month_name=month_name)
-# GDP_API_KEY = os.getenv("GDP_API_KEY")
-# TRAVELS_API_KEY = os.getenv("TRAVELS_API_KEY")
-
-# @places_dashboard_bp.route('/')
-# def places_dashboard_page():
-#     return render_template('places_dashboard/places_dashboard.html')
